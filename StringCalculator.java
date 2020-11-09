d1336428884326d40f7fa826801f764114df2072
Ashish Singh <53008658+ashish0398@users.noreply.github.com>
2020-11-09 23:48:51 +0530

    secondary commit
    properly executed 3rd test case. Made few changes. Using StringBuffer instead of String & StringTokenizer instead of split().

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,28 +1,37 @@
 import java.util.Scanner;
+import java.util.*;
 
 class StringCalculator{
 	private static Scanner scanner = new Scanner(System.in);
+	private static StringBuffer stringBuffer;
+	private static StringTokenizer tokens;
 	
 	public int Add(String input){
 		if(input.equals(" ")|| input.equals("")){
 			System.out.print("No input found.");
 		}
-		String[] numbers = extractNumbers(input);
+		String numbers = extractNumbers(input);
 		
 		return sum(numbers);
 	}
 	
-	private static String[] extractNumbers(String input){
+	private static String extractNumbers(String input){
 		String delimiter = ",";
-		return input.replaceAll("\n", ",").replaceAll(",,", delimiter).split(delimiter);
+		stringBuffer = new StringBuffer(input);
+		do{
+			int index = stringBuffer.indexOf("\\n");
+			stringBuffer.replace(index,index+2,delimiter);
+		}while(stringBuffer.indexOf("\\n")>0);
+		return stringBuffer.toString();
 	}
 	
-	private static int sum(String[] numbers){
+	private static int sum(String numbers){
 		int sum=0;
-		for(String eachNumber : numbers){
-			sum += Integer.parseInt(eachNumber);
+		tokens=new StringTokenizer(numbers,",");
+		
+		while(tokens.hasMoreTokens()){
+			sum += Integer.parseInt(tokens.nextToken());
 		}
-
 		System.out.print("\nSum: "+sum);
 		return sum;
 	}
